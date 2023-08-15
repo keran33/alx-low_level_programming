977aa0c916a1f8561a22740e66dfc063d5e7cf92
Keran Malumbe <ajmalumbe@gmail.com>
2023-08-15 15:27:48 -0700

    display integer sign;didn't use ascii

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - to print the sign of an integer.
+ * @n: the integer to be checked.
+ * Return: 1 if greater than 0, 0 if zero and
+ * -1 if less than zero.
+ */
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+{
+_putchar('-');
+return (-1);
+}
+}
