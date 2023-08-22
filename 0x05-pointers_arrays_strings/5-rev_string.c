30579bea446169dfdb5a33c54a8297d4839da5ad
Keran Malumbe <ajmalumbe@gmail.com>
2023-08-22 03:55:10 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - function to reverse a string
+ * @s: the variable to store it
+ *
+ * Return: is 0.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = 0; i < count / 2; i++)
+	{
+		char j;
+
+		j = s[i];
+		s[i] = s[count - 1 - i];
+		s[count - 1 - i] = j;
+	}
+}
+
