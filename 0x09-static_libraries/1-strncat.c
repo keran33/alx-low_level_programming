e6ae80580106ba4bdf2d27eea17384e300ed2510
Keran Malumbe <ajmalumbe@gmail.com>
2023-09-01 21:55:30 +0000

    static library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenate two strings.
+ * @dest: destination string.
+ * @src: source string.
+ * @n: number of byte to be cat.
+ *
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = *src;
+		src++;
+	}
+	dest[len + i] = '\0';
+
+	return (dest);
+}
