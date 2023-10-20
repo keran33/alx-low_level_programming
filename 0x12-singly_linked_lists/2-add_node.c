528af6d7d4fc6fa2025c5034c4109423e7ba46d3
Keran Malumbe <ajmalumbe@gmail.com>
2023-10-20 01:31:29 +0000

    error somehow.2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,14 +21,16 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	if (*head == NULL)
-
+	{
 		new_node->next = NULL;
+	}
 	else
-		
+	{
 		new_node->next = *head;
 		new_node->str = strdup(str);
 		new_node->len = len(str);
 		*head = new_node;
+	}
 	return (*head);
 }
 /**
